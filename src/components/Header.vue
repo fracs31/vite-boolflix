1e9c5217ca78d59a77a85e9f5301d9d48eb4a16f
fracs31 <pist.fracs95@gmail.com>
2023-03-07 13:50:50 +0100

    ADD logo and search bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,16 @@
 <template>
     <!-- Header -->
     <header class="main-header">
-
+        <!-- Logo -->
+        <div class="logo">
+            <!-- Immagine -->
+            <img class="logo__img" src="../assets/img/logo.png" alt="Logo">
+        </div>
+        <!-- Barra di ricerca -->
+        <div class="search">
+            <!-- Input -->
+            <input class="search__input" type="text" placeholder="Cerca un film o serie tv">
+        </div>
     </header>
 </template>
 
@@ -16,6 +25,22 @@
     .main-header {
         flex-shrink: 0;
         background-color: black;
-        height: 150px;
+        height: 100px;
+        padding: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    /* Immagine usata come logo */
+    .logo__img {
+        width: 150px;
+        object-fit: cover;
+    }
+    /* Input */
+    .search__input {
+        font-size: 16px;
+        width: 400px;
+        height: 40px;
+        padding: 0px 10px;
     }
 </style>
